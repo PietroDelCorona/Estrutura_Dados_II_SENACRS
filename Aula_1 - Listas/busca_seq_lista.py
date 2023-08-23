98b301644dc92efaff30b6bb67649deabe65f35a
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-08-23 18:42:05 -0300

    atualizacao algoritmo busca sequencial

--- Aula_1 - Listas/busca_seq_lista.py
+++ Aula_1 - Listas/busca_seq_lista.py
@@ -2,50 +2,35 @@
 Exercício 1:
 Faça um programa que recebe vetor (lista comum) com 10 inteiros por um valor aleatório"""
 
-
-def busca_sequencial(vetor, elemento):
-    contador = 0
-    for i in range(len(vetor)):
-        contador += 1
-        if vetor[i] == elemento:
-            return True, contador
-    return False, contador
-    
-
-vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-value = int(input("Digite o valor que você deseja procurar: "))
-
-encontrado, contador = busca_sequencial(vetor, value)
-
-if encontrado == True:
-    print(f"Achei! O valor que você procurou era o: {value}")
-else:
-    print(f"Não achei! O valor {value} não está nessa lista")
-
-"""
-Exercício 2:
-Crie uma lista que repete o valor pelo tamanho da lista"""
-print("=================")
 import timeit
-
 
 lista = []
 
-def gerar_lista(tamanho, valor):
-    start = timeit.default_timer()
-    for i in range(tamanho):
-        lista.append(valor)
-    end =timeit.default_timer()
-    print(f"Tempo de iteração da lista é: {float(end - start)}")
+def criar_lista(n, v):
+    for i in range(n):
+        lista.append(v)
     return lista
 
-tamanho = 10
-valor = 1
+def busca_sequencial(l,v):
+    for i in l:
+        if v == l[i]:
+            print("Achou\n")
 
-nova_lista = gerar_lista(tamanho, valor)
-print(nova_lista)
     
+
+v = 1
+tam = 10
+
+lista = criar_lista(tam, v)
+
+print(lista)
+
+start_time = timeit.default_timer()
+
+busca_sequencial(lista, 1)
+
+final_time = timeit.default_timer() - start_time
+print("Tempo: " + str(final_time) + " segundos")
         
         
 
