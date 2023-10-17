645d4dbbac2fd21c028c9a1ae9f31de8dbb5051d
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-10-16 21:38:13 -0300

    algoritmo_heap_sort

---
+++ Algoritmos_trabalho/algoritmo_merge_sort.py
@@ -0,0 +1,64 @@
+import random
+import time
+
+def mergeSort(arr):
+    if len(arr) > 1:
+ 
+         # Finding the mid of the array
+        mid = len(arr)//2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # Into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        mergeSort(L)
+ 
+        # Sorting the second half
+        mergeSort(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+ 
+ 
+# Code to print the list
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+ 
+ 
+# Driver Code
+if __name__ == '__main__':
+    arr = [random.randint(0,500000) for i in range(0,500000)]
+    #print("Given array is")
+    #printList(arr)
+    inicio = time.time()
+    mergeSort(arr)
+    fim = time.time()
+    #print("\nSorted array is ")
+    #printList(arr)
+    tempo_execucao = fim - inicio
+    print(f"Tempo de execução: {tempo_execucao:.6f} segundos")
